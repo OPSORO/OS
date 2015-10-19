ad84f9533b428aa63665b0c5d50a14ddbcb1748d
cesarvandevelde <cesar@Cesars-MacBook-Pro.local>
2015-10-19 11:18:07 +0200

    Removed old commented code

--- OnoSW/apps/configeditor/__init__.py
+++ OnoSW/apps/configeditor/__init__.py
@@ -8,7 +8,6 @@ import time
 
 from flask import Blueprint, render_template, request, send_from_directory
 
-# from expression_manager import ExpressionManager
 from expression import Expression
 
 config = {"full_name": "Config Editor", "icon": "fa-pencil"}
@@ -34,17 +33,6 @@ def setup_pages(onoapp):
 			data["file_functions"] = f.read()
 		return onoapp.render_template("configeditor.html", **data)
 
-	# @configeditor_bp.route("/getdefault/<filename>")
-	# @onoapp.app_api
-	# def getdefault(filename):
-	# 	if filename in ["pinmap", "limits", "functions"]:
-	# 		file = ""
-	# 		with open(get_path("../../config/%s.default.yaml" % filename)) as f:
-	# 			file = f.read()
-	# 		return {"file": file}
-	# 	else:
-	# 		return {"error": "Unknown file."}
-
 	@configeditor_bp.route("/default/<configfile>")
 	@onoapp.app_view
 	def default(configfile):
@@ -60,15 +48,6 @@ def setup_pages(onoapp):
 		pinmap_yaml = request.form.get("pinmap", type=str, default="")
 		limits_yaml = request.form.get("limits", type=str, default="")
 		functions_yaml = request.form.get("functions", type=str, default="")
-
-		# print "Pinmap:"
-		# print pinmap_yaml
-		#
-		# print "Limits:"
-		# print limits_yaml
-		#
-		# print "Functions:"
-		# print functions_yaml
 
 		# Back up old config files.
 		for filename in ["pinmap.yaml", "limits.yaml", "functions.yaml"]:
@@ -102,35 +81,6 @@ def setup_pages(onoapp):
 			return {"status": "error", "message": "Error parsing configuration files:<br/> %s<br><br> Previous configuration was restored." % err_msg}
 		else:
 			return {"message": "Successfully saved and parsed configuration files.<br>Loaded %d DOFs and %d servos." % (len(Expression.servos), len(Expression.dofs))}
-
-
-	# @configeditor_bp.route("/saveconfig/<filename>", methods=["POST"])
-	# @onoapp.app_api
-	# def saveconfig(filename):
-	# 	file = request.form.get("file", type=str, default="")
-	#
-	# 	if filename in ["pinmap", "limits", "functions"]:
-	# 		# Save a backup for logging and for parsing
-	# 		backup_filename = "%s__%s.yaml" % (filename, time.strftime("%Y-%m-%d_%H-%M-%S"))
-	# 		backup_full_path = get_path("../../../OnoSW_backups/%s" % backup_filename)
-	# 		with open(backup_full_path, "w") as f:
-	# 			f.write(file)
-	#
-	# 		try:
-	# 			kwargs = {}
-	# 			kwargs[filename] = ("../../OnoSW_backups/%s" % backup_filename)
-	#
-	# 			# Make an ExpressionManager instance to evaluate new config file
-	# 			em = ExpressionManager(onoapp.hw, **kwargs)
-	#
-	# 			# No exception, so save the new config file
-	# 			with open(get_path("../../config/%s.yaml" % filename), "w") as f:
-	# 				f.write(file)
-	# 		except Exception, e:
-	# 			return {"status": "error", "message": "Error saving config/%s.yaml:<br/> %s" % (filename, str(e))}
-	# 		return {"message": "Successfully saved config/%s.yaml" % filename}
-	# 	else:
-	# 		return {"status": "error", "message": "Unknown file."}
 
 	onoapp.register_app_blueprint(configeditor_bp)
 
