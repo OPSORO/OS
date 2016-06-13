73bd9130280cad5187b32915df40152e4045b427
Hypickles <jens_ameye@outlook.com>
2016-06-13 16:03:26 +0200

    Added exceptionhandling

--- OnoSW/apps/visprog/static/custom_blocks/detection.js
+++ OnoSW/apps/visprog/static/custom_blocks/detection.js
@@ -1,6 +1,6 @@
 Blockly.Lua.addReservedWords("Detection");
 
-Blockly.Blocks['detection_follow'] = {
+Blockly.Blocks['detection_follow_color'] = {
   init: function() {
     var colour = new Blockly.FieldColour('#ff0000');
     colour.setColours(['#f00','#0f0','#00f','#ff0']).setColumns(2);
@@ -10,25 +10,28 @@ Blockly.Blocks['detection_follow'] = {
         .appendField('is detected.');
     this.appendField
     this.setColour(330);
-    this.setTooltip('This will make the robot follow a color, using his eyes.');
+    this.setTooltip('This will make the robot look at a certain color and follow it with his eyes.');
     this.appendDummyInput()
-        .appendField('Follow it');
+        .appendField('Follow color with eyes');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
   }
 };
-Blockly.Lua['detection_follow'] = function(block) {
+Blockly.Lua['detection_follow_color'] = function(block) {
   var color = block.getFieldValue('COLOUR')
   var code = '';
-  code += 'if Detection:is_color("'+color+'") then\n';
-  code += 'x = Detection:get_coord_x("'+color+'")\n';
-  code += 'y = Detection:get_coord_y("'+color+'")\n';
-  code += 'Detection:folow_object(x,y)\n';
+  code += 'if (Detection:is_color_detected("'+color+'") == null) then\n';
+  code += 'print("Something went wrong. Did you perhaps forget to start the camera? (or did you get an error earlier in the script?)")';
+  code += 'end\n';
+  code += 'if Detection:is_color_detected("'+color+'") then\n';
+  code += 'x = Detection:get_color_coord_x("'+color+'")\n';
+  code += 'y = Detection:get_color_coord_y("'+color+'")\n';
+  code += 'Detection:follow_object(x,y)\n';
   code += 'end\n';
   return code;
 };
 
-Blockly.Blocks['detection_get_coordinates'] = {
+Blockly.Blocks['detection_get_coordinates_color'] = {
   init: function() {
     var colour = new Blockly.FieldColour('#ff0000');
     colour.setColours(['#f00','#0f0','#00f','#ff0']).setColumns(2);
@@ -38,49 +41,58 @@ Blockly.Blocks['detection_get_coordinates'] = {
         .appendField('is detected.');
     this.appendField
     this.setColour(330);
-    this.setTooltip('This statement will check for coordinates of a certain color.');
+    this.setTooltip('This block will return the coordinates of a detected color.');
         this.appendDummyInput()
             .appendField(new Blockly.FieldDropdown([["Get y value", "Y"], ["Get x value", "X"]]), "DROPDOWN");
     this.setOutput(true, 'Number');
-    //this.setPreviousStatement(true);
-    //this.setNextStatement(true);
   }
 };
-Blockly.Lua['detection_get_coordinates'] = function(block) {
+Blockly.Lua['detection_get_coordinates_color'] = function(block) {
   var color = block.getFieldValue('COLOUR')
   var dropdown = block.getFieldValue('DROPDOWN');
   var code = '';
-  code += 'if Detection:is_color("'+color+'") then\n';
   if(dropdown == "Y"){
-    code += 'return Detection:get_coord_y("'+color+'")\n';
+    code += 'if (Detection:is_color_detected("'+color+'") == null) then\n';
+    code += 'print("Something went wrong. Did you perhaps forget to start the camera?")';
+    code += 'end\n';
+    code += 'if (Detection:is_color_detected("'+color+'") == false) then\n';
+    code += 'print("No color with hexcode '+color+' detected. Make sure to use an if block to see if a color is detected.")';
+    code += 'end\n';
+    code = 'Detection:get_coord_y("'+color+'")\n';
   }else if(dropdown == "X"){
-    code += 'Detection:get_coord_x("'+color+'")\n';
+    code += 'if (Detection:is_color_detected("'+color+'") == null) then\n';
+    code += 'print("Something went wrong. Did you perhaps forget to start the camera? (or did you get an error earlier in the script?)")';
+    code += 'end\n';
+    code = 'Detection:get_coord_x("'+color+'")\n';
   }
-  code += 'end\n';
-  return code
+  return [code, Blockly.Lua.ORDER_FUNCTION_CALL];
 };
 
 Blockly.Blocks['detection_get_coordinates_face'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Eyes follow face");
+        .appendField("Follow face with eyes");
     this.setColour(330);
-    this.setTooltip('This will make the robot follow a face, using his eyes.');
+    this.setTooltip('This will make the robot look at a face and follow it with his eyes.');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
   }
 };
 Blockly.Lua['detection_get_coordinates_face'] = function(block) {
   var code = '';
+  code += 'if (Detection:is_face_detected() == null) then\n';
+  code += 'print("Something went wrong. Did you perhaps forget to start the camera? (or did you get an error earlier in the script?)")';
+  code += 'end\n';
+  code += 'if (Detection:is_face_detected("'+color+'") == false) then\n';
+  code += 'print("No faces detected. Make sure to use an if block to see if a face is detected.")';
+  code += 'end\n';
   code += 'x = Detection:get_face_coord_x()\n';
   code += 'y = Detection:get_face_coord_y()\n';
-  /*code += 'print(x+"")\n'
-  code += 'print(y+"")\n'*/
-  code += 'Detection:folow_object(x,y)\n';
+  code += 'Detection:follow_object(x,y)\n';
   return code;
 };
 
-Blockly.Blocks['detection_colorCheck'] = {
+Blockly.Blocks['detection_color_check'] = {
   init: function() {
     var colour = new Blockly.FieldColour('#ff0000');
     colour.setColours(['#f00','#0f0','#00f','#ff0']).setColumns(2);
@@ -90,54 +102,75 @@ Blockly.Blocks['detection_colorCheck'] = {
         .appendField('is detected.');
     this.appendField
     this.setColour(330);
-    this.setTooltip('This block will check if the robot detects a certain color.');
+    this.setTooltip('This block will check if the chosen color is detected.');
     this.appendStatementInput('BODY')
     .appendField('do');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
   }
 };
-Blockly.Lua['detection_colorCheck'] = function(block) {
+Blockly.Lua['detection_color_check'] = function(block) {
   var statements_body = Blockly.Lua.statementToCode(block, 'BODY');
   var color = block.getFieldValue('COLOUR')
   this.setFieldValue(color,'COLOUR')
   var code = '';
-  //code += 'print(Detection:is_color("'+color+'"))\n';
+  code += 'if (Detection:is_color_detected("'+color+'") == null) then\n';
+  code += 'print("Something went wrong. Did you perhaps forget to start the camera? (or did you get an error earlier in the script?)")';
+  code += 'end\n';
   code += 'if Detection:is_color("'+color+'") then\n';
   code += statements_body;
   code += 'end\n';
   return code;
 };
-
-
-/*simpel blockje start stream*/
 Blockly.Blocks['detection_start_stream'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Start camera");
     this.setColour(330);
-    this.setTooltip('This will start the camera for further actions.');
+    this.setTooltip('This will start the camera.');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
   }
 };
 Blockly.Lua['detection_start_stream'] = function(block) {
-  var code = 'Detection:start_stream()\n'
+  var code = '';
+  code += 'if (Detection:start_stream() == null) then\n';
+  code += 'print("Something went wrong. Is the camera broken, disabled or disconnected? (or did you get an error earlier in the script?)")\n';
+  code += 'else Detection:start_stream()\n'
+  code += 'end\n';
   return code;
 };
-
-/*simpel blockje stop stream*/
 Blockly.Blocks['detection_stop_stream'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Stop camera");
     this.setColour(330);
-    this.setTooltip('This will stop the camera. THIS IS IMPORTANT!');
+    this.setTooltip('This will stop the camera. IMPORTANT: This is required for optimal experience.');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
   }
 };
 Blockly.Lua['detection_stop_stream'] = function(block) {
   var code = 'Detection:stop_stream()\n'
+  code += 'if (Detection:stop_stream() == null) then\n';
+  code += 'print("Something went wrong. Is the camera broken, disabled or disconnected? (or did you get an error earlier in the script?)")';
+  code += 'end\n';
+  return code;
+};
+Blockly.Blocks['detection_initialize_predictor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Start predicting faces");
+    this.setColour(330);
+    this.setTooltip('This will start loading a script that will predict possible faces and gather their coordinates. THIS IS NEEDED FOR OTHER FACE RELATED CODING!');
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+  }
+};
+Blockly.Lua['detection_initialize_predictor'] = function(block) {
+  var code = 'Detection:initialize_predictor()\n'
+  code += 'if (Detection:initialize_predictor() == null) then\n';
+  code += 'print("Something went wrong. There is a corrupt file path inside the filesystem. Contact a developer for more info.")';
+  code += 'end\n';
   return code;
 };
--- OnoSW/detection.py
+++ OnoSW/detection.py
@@ -16,136 +16,195 @@ import sys
 class _Detection(object):
     vs = None
     def start_stream(self):
-        global vs
-        PiCamera.vflip = True
-        vs = PiVideoStream().start()
-        print("Camera opstarten...")
-        time.sleep(1)
-        print("Camera streaming gestart!")
-        return vs
+        try:
+            global vs
+            PiCamera.vflip = False
+            vs = PiVideoStream().start()
+            time.sleep(1)
+            return vs
+        except Exception as e:
+            return None
 
     def stop_stream(self):
-        global vs
-        vs.stop()
-        print("De camera is gestopt!")
+        try:
+        	global vs
+        	vs.stop()
+        except Exception as e:
+            return None
 
-    def is_color(self,color):
-        global vs
-        def check_for_color(vs):
-            frame = vs.read()
-            frame = imutils.resize(frame, width=400)
-            if args["display"] > 0:
-                blurred = cv2.GaussianBlur(frame, (11,11),0)
-                mask = cv2.inRange(frame, colorLower, colorUpper)
-                mask = cv2.erode(mask, None, iterations=2)
-                mask = cv2.dilate(mask, None, iterations=2)
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-                if len(cnts) > 0:
-                    return True
-                else:
-                    return False
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
-        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
-        args = vars(ap.parse_args())
-        if color == "#ff0000":
-            colorLower = np.array([130, 69, 6])
-            colorUpper = np.array([178, 255,255])
-        if color == "#00ff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        if color == "#0000ff":
-            colorLower = np.array([280, 86, 6])
-            colorUpper = np.array([150, 255,255])
-        if color == "#ffff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-
-        pts = deque(maxlen=args["buffer"])
-        return check_for_color(vs)
-
-    def get_coord_x(self, color):
-        global vs
-        def check_for_green_coord_x(vs):
-            frame = vs.read()
-            frame = imutils.resize(frame, width=350)
-            if args["display"] > 0:
-                blurred = cv2.GaussianBlur(frame, (11,11),0)
-                mask = cv2.inRange(frame, colorLower, colorUpper)
-                mask = cv2.erode(mask, None, iterations=2)
-                mask = cv2.dilate(mask, None, iterations=2)
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-                if len(cnts) > 0:
-                    c = max(cnts, key=cv2.contourArea)
-                    ((x,y),radius) = cv2.minEnclosingCircle(c)
-                    if radius > 0:
-                        return x
+    def is_color_detected(self,color):
+        try:
+            global vs
+            def check_for_color(vs):
+                frame = vs.read()
+                frame = imutils.resize(frame, width=400)
+                if args["display"] > 0:
+                    blurred = cv2.GaussianBlur(frame, (11,11),0)
+                    mask = cv2.inRange(frame, colorLower, colorUpper)
+                    mask = cv2.erode(mask, None, iterations=2)
+                    mask = cv2.dilate(mask, None, iterations=2)
+                    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+                    if len(cnts) > 0:
+                        return True
                     else:
-                        return 175
-                else:
-                    return 175
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
-        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
-        args = vars(ap.parse_args())
-        if color == "#ff0000":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        if color == "#00ff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        if color == "#0000ff":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        if color == "#ffff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        pts = deque(maxlen=args["buffer"])
-        x = check_for_green_coord_x(vs)
-        return x
+                        return False
+            ap = argparse.ArgumentParser()
+            ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
+            ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
+            args = vars(ap.parse_args())
+            if color == "#ff0000":
+                colorLower = np.array([145, 69, 20])
+                colorUpper = np.array([192, 255,255])
+            if color == "#00ff00":
+                colorLower = np.array([29, 86, 20])
+                colorUpper = np.array([64, 255,255])
+            if color == "#0000ff":
+                colorLower = np.array([100, 86, 20])
+                colorUpper = np.array([130, 255,255])
+            if color == "#ffff00":
+                colorLower = np.array([20, 86, 20])
+                colorUpper = np.array([40, 255,255])
 
-    def get_coord_y(self, color):
-        global vs
-        def check_for_green_coord_y(vs):
-            frame = vs.read()
-            frame = imutils.resize(frame, width=350)
-            if args["display"] > 0:
-                blurred = cv2.GaussianBlur(frame, (11,11),0)
-                mask = cv2.inRange(frame, colorLower, colorUpper)
-                mask = cv2.erode(mask, None, iterations=2)
-                mask = cv2.dilate(mask, None, iterations=2)
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-                if len(cnts) > 0:
-                    c = max(cnts, key=cv2.contourArea)
-                    ((x,y),radius) = cv2.minEnclosingCircle(c)
-                    if radius > 0:
-                        return y
-                    else:
-                        return 175
-                else:
-                    return 175
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
-        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
-        args = vars(ap.parse_args())
-        if color == "#ff0000":
-            colorLower = np.array([350, 86, 6])
-            colorUpper = np.array([300, 255,255])
-        if color == "#00ff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        if color == "#0000ff":
-            colorLower = np.array([280, 86, 6])
-            colorUpper = np.array([150, 255,255])
-        if color == "#ffff00":
-            colorLower = np.array([29, 86, 6])
-            colorUpper = np.array([64, 255,255])
-        pts = deque(maxlen=args["buffer"])
-        y = check_for_green_coord_y(vs)
-        return y
+            pts = deque(maxlen=args["buffer"])
+            return check_for_color(vs)
+        except Exception as e:
+            return None
 
-    def get_face_coord_x(self):
-        global vs
+
+    def get_color_coords(self, color):
+        try:
+        	global vs
+	        def check_for_color_coords(vs):
+	            arrCoords = [175,175]
+	            frame = vs.read()
+	            frame = imutils.resize(frame, width=350)
+	            if args["display"] > 0:
+	                blurred = cv2.GaussianBlur(frame, (11,11),0)
+	                mask = cv2.inRange(frame, colorLower, colorUpper)
+	                mask = cv2.erode(mask, None, iterations=2)
+	                mask = cv2.dilate(mask, None, iterations=2)
+	                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	                if len(cnts) > 0:
+	                    c = max(cnts, key=cv2.contourArea)
+	                    ((x,y),radius) = cv2.minEnclosingCircle(c)
+	                    if radius > 0:
+	                        arrCoords[0] = x
+	                        arrCoords[1] = y
+	                        return arrCoords
+	                    else:
+	                        return arrCoords
+	                else:
+	                    return arrCoords
+	        ap = argparse.ArgumentParser()
+	        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
+	        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
+	        args = vars(ap.parse_args())
+	        if color == "#ff0000":
+	            colorLower = np.array([145, 69, 20])
+	            colorUpper = np.array([192, 255,255])
+	        if color == "#00ff00":
+	            colorLower = np.array([29, 86, 20])
+	            colorUpper = np.array([64, 255,255])
+	        if color == "#0000ff":
+	            colorLower = np.array([100, 86, 20])
+	            colorUpper = np.array([130, 255,255])
+	        if color == "#ffff00":
+	            colorLower = np.array([20, 86, 20])
+	            colorUpper = np.array([40, 255,255])
+	        pts = deque(maxlen=args["buffer"])
+	        arrCoords = check_for_color_coords(vs)
+	        return arrCoords
+        except Exception as e:
+            return None
+
+    def get_color_coord_x(self, color):
+        try:
+        	global vs
+	        def check_for_color_coord_x(vs):
+	            frame = vs.read()
+	            frame = imutils.resize(frame, width=350)
+	            if args["display"] > 0:
+	                blurred = cv2.GaussianBlur(frame, (11,11),0)
+	                mask = cv2.inRange(frame, colorLower, colorUpper)
+	                mask = cv2.erode(mask, None, iterations=2)
+	                mask = cv2.dilate(mask, None, iterations=2)
+	                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	                if len(cnts) > 0:
+	                    c = max(cnts, key=cv2.contourArea)
+	                    ((x,y),radius) = cv2.minEnclosingCircle(c)
+	                    if radius > 0:
+	                        return x
+	                    else:
+	                        return 175
+	                else:
+	                    return 175
+	        ap = argparse.ArgumentParser()
+	        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
+	        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
+	        args = vars(ap.parse_args())
+	        if color == "#ff0000":
+	            colorLower = np.array([145, 69, 20])
+	            colorUpper = np.array([192, 255,255])
+	        if color == "#00ff00":
+	            colorLower = np.array([29, 86, 20])
+	            colorUpper = np.array([64, 255,255])
+	        if color == "#0000ff":
+	            colorLower = np.array([100, 86, 20])
+	            colorUpper = np.array([130, 255,255])
+	        if color == "#ffff00":
+	            colorLower = np.array([20, 86, 20])
+	            colorUpper = np.array([40, 255,255])
+	        pts = deque(maxlen=args["buffer"])
+	        x = check_for_color_coord_x(vs)
+	        return x
+        except Exception as e:
+            return None
+
+    def get_color_coord_y(self, color):
+        try:
+        	global vs
+	        def check_for_color_coord_y(vs):
+	            frame = vs.read()
+	            frame = imutils.resize(frame, width=350)
+	            if args["display"] > 0:
+	                blurred = cv2.GaussianBlur(frame, (11,11),0)
+	                mask = cv2.inRange(frame, colorLower, colorUpper)
+	                mask = cv2.erode(mask, None, iterations=2)
+	                mask = cv2.dilate(mask, None, iterations=2)
+	                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	                if len(cnts) > 0:
+	                    c = max(cnts, key=cv2.contourArea)
+	                    ((x,y),radius) = cv2.minEnclosingCircle(c)
+	                    if radius > 0:
+	                        return y
+	                    else:
+	                        return 175
+	                else:
+	                    return 175
+	        ap = argparse.ArgumentParser()
+	        ap.add_argument("-d", "--display", type=int, default=1, help="Wheter or not frames should be displayed")
+	        ap.add_argument("-b", "--buffer", type=int, default=2500, help="Max buffer size")
+	        args = vars(ap.parse_args())
+	        if color == "#ff0000":
+	            colorLower = np.array([145, 69, 20])
+	            colorUpper = np.array([192, 255,255])
+	        if color == "#00ff00":
+	            colorLower = np.array([29, 86, 20])
+	            colorUpper = np.array([64, 255,255])
+	        if color == "#0000ff":
+	            colorLower = np.array([100, 86, 20])
+	            colorUpper = np.array([130, 255,255])
+	        if color == "#ffff00":
+	            colorLower = np.array([20, 86, 20])
+	            colorUpper = np.array([40, 255,255])
+	        pts = deque(maxlen=args["buffer"])
+	        y = check_for_color_coord_y(vs)
+	        return y
+        except Exception as e:
+            return None
+
+    def get_face_coords(self):
+        arrCoords = [175,175]
         face = cv2.CascadeClassifier('data/visprog/scripts/haarcascade_frontalface_default')
         frame = vs.read()
         frame = imutils.resize(frame, width=350)
@@ -153,39 +212,105 @@ class _Detection(object):
         facedet = face.detectMultiScale(gray, 1.3,5)
         if facedet != ():
             for(x,y,w,h) in facedet:
-                xValue = x + (w/2)
-                return xValue
+                arrCoords[0] = x + (w/2)
+                arrCoords[1] = y + (h/2)
+                return arrCoords
         else:
-            xValue = 175
-            return xValue
+            return arrCoords
+
+    def get_face_coord_x(self):
+        try:
+        	face = cv2.CascadeClassifier('data/visprog/scripts/haarcascade_frontalface_default')
+	        frame = vs.read()
+	        frame = imutils.resize(frame, width=350)
+	        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	        facedet = face.detectMultiScale(gray, 1.3,5)
+	        if facedet != ():
+	            for(x,y,w,h) in facedet:
+	                xValue = x + (w/2)
+	                return xValue
+	        else:
+	            xValue = 175
+	            return xValue
+        except Exception as e:
+            return None
 
     def get_face_coord_y(self):
-        global vs
-        face = cv2.CascadeClassifier('data/visprog/scripts/haarcascade_frontalface_default')
-        frame = vs.read()
-        frame = imutils.resize(frame, width=350)
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        facedet = face.detectMultiScale(gray, 1.3,5)
-        if facedet !=():
-            for(x,y,w,h) in facedet:
-                yValue = y + (h/2)
-                return yValue
-        else:
-            yValue = 175
-            return yValue
+        try:
+        	face = cv2.CascadeClassifier('data/visprog/scripts/haarcascade_frontalface_default')
+	        frame = vs.read()
+	        frame = imutils.resize(frame, width=350)
+	        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	        facedet = face.detectMultiScale(gray, 1.3,5)
+	        if facedet !=():
+	            for(x,y,w,h) in facedet:
+	                yValue = y + (h/2)
+	                return yValue
+	        else:
+	            yValue = 175
+	            return yValue
+        except Exception as e:
+            return None
 
-    def folow_object(self,xCoord,yCoord):
-        rechter_x = 1350 + (xCoord * 2)
-        rechter_y = 1875 - (yCoord * 2)
 
-        linker_x = 1200 + (xCoord * 2)
-        linker_y = 1175 + (yCoord * 2)
+    def initialize_predictor(self):
+        try:
+        	global predictor
+        	predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        except Exception as e:
+            return None
 
-        Hardware.servo_set(3,int(rechter_x))
-        Hardware.servo_set(2,int(rechter_y))
 
-        Hardware.servo_set(13, int(linker_x))
-        Hardware.servo_set(12, int(linker_y))
+    def receive_face_points(self, numberLandmark):
+        try:
+        	global vs
+	        global predictor
+	        def get_landmarks(im):
+	            rects = cascade.detectMultiScale(im, 1.3,5)
+	            x,y,w,h = rects[0]
+	            if len(rects) >= 1:
+	                rect=dlib.rectangle(int(x),int(y),int(x+w),int(y+h))
+	                return np.matrix([[p.x,p.y] for p in predictor(im, rect).parts()])
+
+	        def annotate_landmarks(im, landmarks, numberLandmark):
+	            arrCoords = [175,175]
+	            for idx, point in enumerate(landmarks):
+	                pos = (point[0, 0], point[0, 1])
+	                if idx == numberLandmark:
+	                    arrCoords[0] = landmarks[numberLandmark,0]
+	                    arrCoords[1] = landmarks[numberLandmark,1]
+	            return arrCoords
+	        frame = vs.read()
+	        cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+	        frame = imutils.resize(frame, width=350)
+	        blurred = cv2.GaussianBlur(frame, (11,11),0)
+	        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	        try:
+	            landmarks = get_landmarks(frame)
+	            arrCoords = annotate_landmarks(frame, landmarks, numberLandmark)
+	            return arrCoords
+
+	        except:
+	            return [175,175]
+        except Exception as e:
+            return None
+
+
+    def follow_object(self,xCoord,yCoord):
+        try:
+        	rechter_x = 1350 + (xCoord * 2)
+	        rechter_y = 1875 - (yCoord * 2)
+
+	        linker_x = 1200 + (xCoord * 2)
+	        linker_y = 1175 + (yCoord * 2)
+
+	        Hardware.servo_set(3,int(rechter_x))
+	        Hardware.servo_set(2,int(rechter_y))
+
+	        Hardware.servo_set(13, int(linker_x))
+	        Hardware.servo_set(12, int(linker_y))
+        except Exception as e:
+            return None
 
 		#reghter oog horizontaal
 		#Hardware.servo_set(3,1350)
