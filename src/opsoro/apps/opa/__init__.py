33d0b7fa324db50843d9fdd7c6266b74e3eaa73c
Ruben Verschuere <ruben.verschuere@hotmail.com>
2017-05-30 06:48:33 -0700

    Applica Added

---
+++ src/opsoro/apps/opa/__init__.py
@@ -0,0 +1,66 @@
+from __future__ import with_statement
+
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
+
+from opsoro.console_msg import *
+from opsoro.hardware import Hardware
+from opsoro.robot import Robot
+from opsoro.expression import Expression
+# from opsoro.stoppable_thread import StoppableThread
+from opsoro.sound import Sound
+
+from functools import partial
+import os
+
+constrain = lambda n, minn, maxn: max(min(maxn, n), minn)
+get_path = partial(os.path.join, os.path.abspath(os.path.dirname(__file__)))
+
+config = {
+    'full_name':            'opsoro personal assistant',
+    'icon':                 'fa-hand-spock-o',
+    'color':                'green',
+    'difficulty':           1,
+    'tags':                 ['template', 'developer'],
+    'allowed_background':   False,
+    'connection':           Robot.Connection.OFFLINE,
+    'activation':           Robot.Activation.AUTO
+}
+config['formatted_name'] =  config['full_name'].lower().replace(' ', '_')
+
+def setup_pages(server):
+    app_bp = Blueprint(config['formatted_name'], __name__, template_folder='templates', static_folder='static')
+
+    # Public function declarations
+    app_bp.add_url_rule('/demo',    'demo',     server.app_api(demo),       methods=['GET', 'POST'])
+
+    @app_bp.route('/')
+    @server.app_view
+    def index():
+        data = {
+            'actions': {},
+            'data': [],
+        }
+        action = request.args.get('action', None)
+        if action != None:
+            data['actions'][action] = request.args.get('param', None)
+
+        return server.render_template(config['formatted_name'] + '.html', **data)
+
+    server.register_app_blueprint(app_bp)
+
+def demo():
+    # publicly accessible function
+    if 1 > 0:
+        return {'status': 'success'}
+    else:
+        return {'status': 'error', 'message': 'This is a demo error!'}
+
+# Default functions for setting up, starting and stopping an app
+def setup(server):
+    pass
+
+def start(server):
+    pass
+
+def stop(server):
+    pass
---
+++ src/opsoro/apps/opa/static/app.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    var Model = function() {
+        var self = this;
+
+        // File operations toolbar item
+        self.fileIsLocked = ko.observable(false);
+        self.fileIsModified = ko.observable(false);
+        self.fileName = ko.observable("");
+        self.fileStatus = ko.observable("");
+        self.fileExtension = ko.observable(".ext");
+
+        // Script operations toolbar item
+        self.isRunning = ko.observable(false);
+        self.isUI = ko.observable(false);
+
+        // Lock/Unlock toolbar item
+        self.toggleLocked = function() {
+            if (self.fileIsLocked()) {
+                self.unlockFile();
+            } else {
+                self.lockFile();
+            }
+        };
+        self.lockFile = function() {
+            self.fileIsLocked(true);
+            self.fileStatus("Locked")
+        };
+        self.unlockFile = function() {
+            self.fileIsLocked(false);
+            self.fileStatus("Editing")
+        };
+
+        // Popup window
+        self.popupTextInput = ko.observable("Hi! This text can be changed. Click on the button to change me!");
+        self.showPopup = function() {
+          $("#popup_window").foundation('open');
+
+        };
+        self.closePopup = function() {
+            $("#popup_window").foundation('close');
+        };
+        self.popupButtonHandler = function() {
+            self.closePopup();
+        };
+
+        self.init = function() {
+            // Clear data, new file, ...
+            self.fileName("Untitled");
+            self.unlockFile();
+            self.fileIsModified(false);
+        };
+
+        self.loadFileData = function(data) {
+            if (data == undefined) {
+                return;
+            }
+
+            // Load data, parse if needed
+            var dataobj = JSON.parse(data);
+
+
+            self.fileIsModified(false);
+            self.lockFile();
+        };
+
+        self.saveFileData = function() {
+            // Convert data
+            file_data = {};
+
+            var data = ko.toJSON(file_data, null, 2);
+            self.fileIsModified(false);
+            return data;
+        };
+    };
+    // This makes Knockout get to work
+    var model = new Model();
+    ko.applyBindings(model);
+    model.fileIsModified(false);
+
+    // Configurate toolbar handlers
+    //config_file_operations("", model.fileExtension(), model.saveFileData, model.loadFileData, model.init);
+});
