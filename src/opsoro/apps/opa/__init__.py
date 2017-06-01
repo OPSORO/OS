6211a08c18cb4a4edf885aada3267b866ccdb882
root <ruben.verschuere@hotmail.com>
2017-06-01 16:28:59 +0200

    IFTTT integratie

--- src/opsoro/apps/opa/__init__.py
+++ src/opsoro/apps/opa/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import with_statement
 
+import paho.mqtt.client as mqtt
+import json
 from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
 
 from opsoro.console_msg import *
@@ -29,9 +31,9 @@ config['formatted_name'] =  'opa'
 
 def setup_pages(server):
     app_bp = Blueprint(config['formatted_name'], __name__, template_folder='templates', static_folder='static')
-
     # Public function declarations
     app_bp.add_url_rule('/demo',    'demo',     server.app_api(demo),       methods=['GET', 'POST'])
+
 
     @app_bp.route('/')
     @server.app_view
@@ -46,7 +48,43 @@ def setup_pages(server):
 
         return server.render_template(config['formatted_name'] + '.html', **data)
 
+    @app_bp.route('/facebook', methods=['POST'])
+    def tweet():
+        json_dict = request.data
+        data = json.loads(json_dict)
+        print_info("New post from: " + data['From'])
+        print_info("Status: " + data['Message'])
+        Sound.play_file("smb_1-up.wav")
+        Sound.say_tts(data['From'] + "posted a new status on facebook ")
+        Sound.wait_for_sound()
+        Sound.say_tts(data['Message'])
+        return redirect('/apps/opa/')
+
+    @app_bp.route('/space', methods=['GET'])
+    def space():
+        Sound.play_file("Ahhh-surprise.wav")
+        Sound.wait_for_sound()
+        Sound.say_tts("Hallo Ruben, ik heb een nieuwe melding")
+        Sound.wait_for_sound()
+        Sound.say_tts("International space station is juist boven u gepasseerd")
+        return redirect('/apps/opa')
+
+    @app_bp.route('/event', methods=['POST'])
+    def event():
+        json_dict = request.data
+        data = json.loads(json_dict)
+        print_info(data['Title'])
+        print_info(data['Location'])
+        Sound.play_file("Burp.wav")
+        Sound.wait_for_sound()
+        Sound.say_tts("Je hebt een evenement in vijftien minuten")
+        Sound.wait_for_sound()
+        Sound.say_tts("Titel evenement" + data['Title'])
+        Sound.wait_for_sound()
+        Sound.say_tts("Locatie" + data['Location'])
+        return redirect('/apps/opa')
     server.register_app_blueprint(app_bp)
+
 
 def demo():
     # publicly accessible function
