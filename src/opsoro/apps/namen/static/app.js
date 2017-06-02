de8672d73aa799068b5f9300201902950ea234d0
Ellen <ellentaillieu@hotmail.com>
2017-06-02 15:42:36 +0200

    add icon contacts

--- src/opsoro/apps/namen/static/app.js
+++ src/opsoro/apps/namen/static/app.js
@@ -1,32 +1,3 @@
 $(document).ready(function () {
 
-  // data ophalen idk...
-  var Userscontact = function(){
-    var self = this;
-
-    self.personName = ko.observable('');
-    self.personNumber = ko.observable('');
-
-  };
-
-  // model
-  ContactModel = function(){
-    var self = this;
-
-    self.users = ko.observable(new Userscontact());
-  }
-
-  //doorgeven???
-  var viewmodel = new contactModel();
-  ko.applyBindings(viewmodel);
-  viewmodel.Users().
-
-
-
-//  var person = { name: "elleneke",
-//                  number: 034536326346
-//    };
-
-//    ko.applyBindings(person, document.getElementById("person"));
-
-});
+)};
