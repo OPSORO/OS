c2c13b3ac25e0d08c4ec671460a2f13b86e91faf
Jeroen Dellaert <jeroendellaert@hotmail.com>
2017-06-06 10:27:54 +0200

    contacts pagina html + css

--- src/opsoro/apps/namen/static/app.js
+++ src/opsoro/apps/namen/static/app.js
@@ -5,8 +5,6 @@
 
 	self.conName = ko.observable();
 	self.phone = ko.observable();
-	self.email = ko.observable();
-	self.address = ko.observable();
 
 	self.contacts = ko.observableArray();
 
@@ -27,15 +25,48 @@
         console.log('saving');
 		self.addItem();
 		localStorage.setItem("stored_contacts", ko.toJSON(self.contacts()));
+		// na python
 	};
 
 	ko.applyBindings(self.templateName);
+
+
+
+
+
+	self.templateBan = 'banTemplate';
+
+	self.phoneBan = ko.observable();
+
+	self.ban = ko.observableArray();
+
+	self.addItem = function(){
+		self.ban.push(new Contact(self.conName(), self.phone()));
+	};
+
+	self.removeBanItem = function(item){
+		self.contacts.remove(item);
+	};
+
+	self.load = function(){
+		// haven't tested, but probably won't work. I think you'd have to convert your JSON to observables
+		self.contacts = localStorage.getItem("stored_contacts");
+	};
+
+	self.saveBan = function(){
+        console.log('saving');
+		self.addItem();
+		localStorage.setItem("stored_contacts", ko.toJSON(self.contacts()));
+		// na python
+	};
+
+	ko.applyBindings(self.templateBan);
+
 })();
 
-function Contact(name,phone,email,address){
+function Contact(name,phone){
 	var self = this;
 
 	self.conName = ko.observable(name);
 	self.phone = ko.observable(phone);
 }
-)};
