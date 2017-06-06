a9efc3b83d6cc3352a011dc33fa6018a43533cf2
Jeroen Dellaert <jeroendellaert@hotmail.com>
2017-06-06 13:37:12 +0200

    contacts pagina af - python

--- src/opsoro/apps/namen/static/app.js
+++ src/opsoro/apps/namen/static/app.js
@@ -31,37 +31,6 @@
 	ko.applyBindings(self.templateName);
 
 
-
-
-
-	self.templateBan = 'banTemplate';
-
-	self.phoneBan = ko.observable();
-
-	self.ban = ko.observableArray();
-
-	self.addItem = function(){
-		self.ban.push(new Contact(self.conName(), self.phone()));
-	};
-
-	self.removeBanItem = function(item){
-		self.contacts.remove(item);
-	};
-
-	self.load = function(){
-		// haven't tested, but probably won't work. I think you'd have to convert your JSON to observables
-		self.contacts = localStorage.getItem("stored_contacts");
-	};
-
-	self.saveBan = function(){
-        console.log('saving');
-		self.addItem();
-		localStorage.setItem("stored_contacts", ko.toJSON(self.contacts()));
-		// na python
-	};
-
-	ko.applyBindings(self.templateBan);
-
 })();
 
 function Contact(name,phone){
